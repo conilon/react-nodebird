48d86658acc37ececb61f5186631d7f296096e43
conilon <conilon@naver.com>
2019-10-12 03:17:55 +0900

    1-5. 기본 페이지들 만들기

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import Link from 'next/link';
 import Head from 'next/head';
 import AppLayout from '../components/AppLayout';
 
-const Home = () => {
+const Signup = () => {
     return (
         <>
             <Head>
@@ -11,10 +10,12 @@ const Home = () => {
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.6/antd.css" />
             </Head>
             <AppLayout>
-                <div>Hello, Next!</div>
+                <div>
+                    회원가입
+                </div>
             </AppLayout>
         </>
     );
 };
 
-export default Home;
+export default Signup;
