7fff31756ecf3d2f9ca8fc217f8c177281696aea
conilon <conilon@naver.com>
2019-10-12 12:02:31 +0900

    2-4. 커스텀 훅 재사용하기

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
-import { Menu, Input, Button, Row, Col, Card, Avatar } from 'antd';
+import { Menu, Input, Row, Col, Card, Avatar } from 'antd';
+import LoginForm from './LoginForm';
 
 const dummy ={
     nickname: 'th',
     Post: [],
     Following: [],
     Follower: [],
+    isLoggedIn: false,
 };
 
 const AppLayout = ({ children }) => {
@@ -20,21 +22,24 @@ const AppLayout = ({ children }) => {
                     <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
                 </Menu.Item>
             </Menu>
-            <Link href="/signup"><a><Button>회원가입</Button></a></Link>
             <Row>
                 <Col xs={24} md={6}>
-                    <Card
-                        actions={[
-                            <div key="twit">짹짹<br />{dummy.Post.length}</div>,
-                            <div key="following">팔로잉<br />{dummy.Following.length}</div>,
-                            <div key="follower">팔로워<br />{dummy.Follower.length}</div>,
-                        ]}
-                    >
-                        <Card.Meta 
-                            avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
-                            title={dummy.nickname}
-                        />
-                    </Card>
+                    {dummy.isLoggedIn ? 
+                        <Card
+                            actions={[
+                                <div key="twit">짹짹<br />{dummy.Post.length}</div>,
+                                <div key="following">팔로잉<br />{dummy.Following.length}</div>,
+                                <div key="follower">팔로워<br />{dummy.Follower.length}</div>,
+                            ]}
+                        >
+                            <Card.Meta 
+                                avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+                                title={dummy.nickname}
+                            />
+                        </Card>
+                        :
+                        <LoginForm />
+                    }
                 </Col>
                 <Col xs={24} md={12}>
                     {children}
---
+++ front/components/LoginForm.js
@@ -0,0 +1,45 @@
+import React, { useState, useCallback } from 'react';
+import Link from 'next/link';
+import { Form, Input, Button } from 'antd';
+
+const useInput = (initValue = null) => {
+    const [value, setter] = useState(initValue);
+    const handler = useCallback((e) => {
+        setter(e.target.value);
+    }, []);
+    return [value, handler];
+};
+
+const LoginForm = () => {
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
+
+    const onSubmitForm = useCallback((e) => {
+        e.preventDefault();
+        console.log({
+            id,
+            password,
+        });
+    }, [id, password]);
+
+    return (
+        <Form onSubmit={onSubmitForm}>
+            <div>
+                <label htmlFor="user-id">아이디</label>
+                <br />
+                <Input name="user-id" value={id} onChange={onChangeId} required />
+            </div>
+            <div>
+                <label htmlFor="user-password">비밀번호</label>
+                <br />
+                <Input name="user-password" type="password" value={password} onChange={onChangePassword} required />
+            </div>
+            <div>
+                <Button type="primary" htmlType="submit" loading={false}>로그인</Button>
+                <Link href="/signup"><a><Button type="primary">회원가입</Button></a></Link>
+            </div>
+        </Form>
+    );
+};
+
+export default LoginForm;
