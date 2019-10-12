78d9f75b2025000ca87fe398a8fe1588ee05b64a
conilon <conilon@naver.com>
2019-10-12 11:24:51 +0900

    2-2. prop-types

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 import AppLayout from '../components/AppLayout';
 
 const Nodebird = ({ Component }) => {
@@ -14,6 +15,10 @@ const Nodebird = ({ Component }) => {
             </AppLayout>
         </>
     );
+};
+
+Nodebird.propTypes = {
+    Component: PropTypes.elementType.isRequired,
 };
 
 export default Nodebird;
