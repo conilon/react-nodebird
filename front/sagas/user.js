93be968d987468a7bcbc20685ba952f4f520c22e
conilon <conilon@naver.com>
2019-10-13 11:23:49 +0900

    4-6.  takeEvery, takeLatest

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,5 +1,7 @@
-import { all, fork, takeLatest, call, put, take, delay } from 'redux-saga/effects';
+import { all, fork, takeLatest, takeEvery, call, put, take, delay } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
+
+const HELLO_SAGA = 'HELLO_SAGA';
 
 function loginAPI() {
     // 서버에 요청을 보내는 부분
@@ -29,13 +31,29 @@ function* watchLogin() {
     }
 }
 
-function* watchSignUp() {
-
+function* watchHello() {
+    yield takeLatest(HELLO_SAGA, function*() {
+        yield put({
+            type: 'BYE_SAGA',
+        });
+    });
 }
+
+/* 
+function* watchHello() {
+    while (true) {
+        yield take(HELLO_SAGA);
+        console.log(1);
+        console.log(2);
+        console.log(3);
+        console.log(4);
+        console.log(5);
+    }
+} */
 
 export default function* userSaga() {
     yield all([
         watchLogin(),
-        watchSignUp(),
+        watchHello(),
     ]);
 }
