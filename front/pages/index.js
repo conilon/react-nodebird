5ab02b86ab70a161e9c2756644501086785e9ecc
conilon <conilon@naver.com>
2019-10-13 10:42:07 +0900

    4-5. 사가에서 반복문 제어하기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,25 +1,12 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector} from 'react-redux';
 import PostForm from '../components/PostForm';
 import PostCard from '../components/PostCard';
 
 const Home = () => {
     const { isLoggedIn } = useSelector(state => state.user);
     const { mainPosts } = useSelector(state => state.post);
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch({
-            type: 'HELLO_SAGA',
-        });
-        dispatch({
-            type: 'HELLO_SAGA',
-        });
-        dispatch({
-            type: 'HELLO_SAGA',
-        });
-    }, []);
-
+    
     return (
         <div>
             {isLoggedIn && <PostForm /> }
--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,7 +1,5 @@
-import { all, fork, takeLatest, call, put, take } from 'redux-saga/effects';
+import { all, fork, takeLatest, call, put, take, delay } from 'redux-saga/effects';
 import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from '../reducers/user';
-
-const HELLO_SAGA = 'HELLO_SAGA';
 
 function loginAPI() {
     // 서버에 요청을 보내는 부분
@@ -22,18 +20,22 @@ function* login() {
 }
 
 function* watchLogin() {
-    yield takeLatest(LOG_IN, login);
-}
-
-function* helloSaga() {
-    console.log('before saga');
     while (true) {
-        yield take(HELLO_SAGA);
-        console.log('hello saga');
-        // 비동기 요청, 타이머를 넣어도 된다.
+        yield take(LOG_IN);
+        yield delay(2000);
+        yield put({
+            type: LOG_IN_SUCCESS,
+        });
     }
 }
 
+function* watchSignUp() {
+
+}
+
 export default function* userSaga() {
-    yield helloSaga();
+    yield all([
+        watchLogin(),
+        watchSignUp(),
+    ]);
 }
