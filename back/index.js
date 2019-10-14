8d77ca21f4631f94cdb21b3f9f21b89d84cb656d
conilon <conilon@naver.com>
2019-10-14 19:39:57 +0900

    5-9. 로그인을 위한 미들웨어들

--- back/index.js
+++ back/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
+const dotenv = require('dotenv');
 
 const db = require('./models');
 const userAPIRouter = require('./routes/user');
 const postAPIRouter = require('./routes/post');
 const postsAPIRouter = require('./routes/posts');
 
+dotenv.config();
 const app = express();
 db.sequelize.sync();
 
@@ -14,6 +18,16 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false, // https를 쓸 때 true로 한다.
+    },
+}));
 
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열여둔 창구
 app.use('/api/user', userAPIRouter);
