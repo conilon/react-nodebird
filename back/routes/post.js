6ff87d7f7a3fb6e35e66f1e1b3f159447656f586
conilon <conilon@naver.com>
2019-10-16 20:22:42 +0900

    6-9. multer로 이미지 업로드 받기

--- back/routes/post.js
+++ back/routes/post.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const db = require('../models');
 const { isLoggedIn, exPost } = require('./middleware');
 
@@ -31,8 +33,22 @@ router.post('/', isLoggedIn, async (req, res, next) => { // POST /api/post
     }
 });
 
-router.post('/images', (req, res, next) => {
-
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done) {
+            done(null, 'uploads');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            const basename = path.basename(file.originalname, ext); // th.png, ext === .png, basename === th
+            done(null, basename + new Date().valueOf() + ext);
+        }
+    }),
+    limits: { fileSize: 20 * 1024 * 1024 },
+});
+router.post('/images', upload.array('image'), (req, res, next) => {
+    console.log(req.files);
+    res.json(req.files.map((v) => v.filename));
 });
 
 router.get('/:id/comments', exPost, async (req, res, next) => { // GET /api/post/14/comments
