3b13af332c582b99982a038216e2de90063ac47f
conilon <conilon@naver.com>
2019-10-17 01:54:25 +0900

    6-13. react-slick으로 이미지 슬라이더 구현

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,30 +1,52 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'antd';
+import ImagesZoom from './ImagesZoom';
 
 const PostImages = ({ images }) => {
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+
+    const onZoom = useCallback(() => {
+        setShowImagesZoom(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, []);
+
     if (images.length === 1) {
         return (
-            <img src={`http://localhost:3065/${images[0].src}`} alt="example" />
+            <>
+                <div onClick={onZoom}>
+                    <img src={`http://localhost:3065/${images[0].src}`} alt="example" style={{ width: '100%' }} />
+                </div>
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
         );
     }
     if (images.length === 2) {
         return (
-            <div>
-                <img src={`http://localhost:3065/${images[0].src}`} alt="example" style={{ width: '50%' }} />
-                <img src={`http://localhost:3065/${images[1].src}`} alt="example" style={{ width: '50%' }} />
-            </div>
+            <>
+                <div onClick={onZoom}>
+                    <img src={`http://localhost:3065/${images[0].src}`} alt="example" style={{ width: '50%' }} />
+                    <img src={`http://localhost:3065/${images[1].src}`} alt="example" style={{ width: '50%' }} />
+                </div>
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
         );
     }
     return (
-        <div>
-            <img src={`http://localhost:3065/${images[0].src}`} alt="example" style={{ width: '50%' }} />
-            <div style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}>
-                <Icon type="plus" />
-                <br />
-                {images.length - 1} 개의 사진 더보기
+        <>
+            <div>
+                <img src={`http://localhost:3065/${images[0].src}`} alt="example" style={{ width: '50%' }} />
+                <div onClick={onZoom} style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}>
+                    <Icon type="plus" />
+                    <br />
+                    {images.length - 1} 개의 사진 더보기
+                </div>
             </div>
-        </div>
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+        </>
     );
 };
 
