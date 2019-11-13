392cd3f5ce6573093b7261acffa8b576dd0284a3
conilon <conilon@naver.com>
2019-11-13 10:14:25 +0900

    Modify portfolio

--- back/routes/portfolio.js
+++ back/routes/portfolio.js
@@ -9,6 +9,23 @@ const { isLoggedIn } = require('./middleware');
 
 const router = express.Router();
 
+AWS.config.update({
+    region: 'ap-northeast-2',
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+});
+
+const upload3 = multer({
+    storage: multerS3({
+        s3: new AWS.S3(),
+        bucket: process.env.S3_BUCKET,
+        key(req, file, cb) {
+            cb(null, `original/${+new Date()}${path.basename(file.originalname)}`);
+        },
+    }),
+    limits: { fileSize: 20 * 1024 * 1024 },
+});
+
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, done) {
